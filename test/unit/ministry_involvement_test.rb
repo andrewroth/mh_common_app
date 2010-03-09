45a11a6ea8fefbf54bb28284c5b0b83c7c187bf8
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-03-09 17:45:19 -0500

    Improve test coverage

--- test/unit/ministry_involvement_test.rb
+++ test/unit/ministry_involvement_test.rb
@@ -2,8 +2,13 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class MinistryInvolvementTest < ActiveSupport::TestCase
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_validate
+    Factory(:ministryinvolvement_1)
+    Factory(:ministry_1)
+    Factory(:ministryrole_1)
+    Factory(:person_1)
+    mi = MinistryInvolvement.new(:person_id => 50000, :end_date => nil, :ministry_id => 1, :ministry_role_id => 1)
+    mi.validate
+    assert_equal("There is already a ministry involvement for the ministry \"YFC\"; you can only be involved once per ministry.  Archive the existing involvement and try again.", mi.errors["base"])
   end
 end
