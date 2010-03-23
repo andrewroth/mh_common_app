4824d633bc59b5046d63dcacf46795bf689d7d79
Andrew Roth <andrewroth@gmail.com>
2010-03-23 17:31:13 -0400

    add assignmentstatus_test

---
+++ test/unit/assignmentstatus_test.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class AssignmentstatusTest < ActiveSupport::TestCase
+  def setup
+    setup_assignmentstatuses
+  end
+
+  # make sure associations are mapped correctly
+  def test_associations
+    @assignmentstatus = Assignmentstatus.first
+    @assignmentstatus.assignments
+  end
+
+  def test_find_status_id
+    assert_equal 1, Assignmentstatus.find_status_id("Current Student")
+  end
+end
