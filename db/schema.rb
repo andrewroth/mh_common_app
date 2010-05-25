b0251c52514e2c7cd075156b8daf808e2bd0cca0
Andrew Roth <andrewroth@gmail.com>
2010-05-25 14:53:45 -0400

    make migrate build CDM db

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100426181222) do
+ActiveRecord::Schema.define(:version => 20100305163625) do
 
   create_table "addresses", :force => true do |t|
     t.integer "person_id"
