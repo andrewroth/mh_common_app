2d5e062cf81f50737fb93b2c3811ad72b5ddca81
Andrew Roth <andrewroth@gmail.com>
2010-10-19 11:15:10 -0400

    make regions test pass

--- test/unit/legacy/hrdb/region_test.rb
+++ test/unit/legacy/hrdb/region_test.rb
@@ -8,8 +8,9 @@ class RegionTest < ActiveSupport::TestCase
 
     regions = ::Region.find_regions(Factory(:country_2))
 
-    assert_equal(3, regions.size)
-    assert_equal([["Ontario & Maritimes"], ["Quebec"], ["Western Canada"]], regions)
+    assert_equal(6, regions.size)
+    assert_array_similarity([["Ontario & Maritimes"], ["Quebec"], ["Western Canada"], ["National"],
+    ["Eastern US"], ["Western US"]], regions)
   end
 
   def test_find_region_id
