9dd1d24edb71cb4bf09fd16b9b75ec87584a3a7c
Andrew Roth <andrewroth@gmail.com>
2011-01-14 12:30:24 -0500

    upgrade to 2.3.10 so that mysql2 works properly

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
 
 #Constants file for stats
 require File.join(File.dirname(__FILE__), 'constants')
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "will_paginate", :version => '2.3.15'
+  config.gem "mysql2"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
